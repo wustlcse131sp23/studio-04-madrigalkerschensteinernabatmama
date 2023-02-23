0e9e97c81cb27d86414f1f4209f552d51aa526cd
evmad <evmad@Evelyns_Laptop.dhcp.wustl.edu>
2023-02-23 14:01:29 -0600

    Finished

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,8 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.clear();
+		StdDraw.setPenColor(255,0,0);
+		StdDraw.filledEllipse(0.5,0.5,0.2,0.2);
 	}
 }
